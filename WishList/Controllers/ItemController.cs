6c639a1c809430d6163c6c9ac47ca226c262ddb6
olena-ageyeva <eageeva!gmail.com>
2019-10-28 07:16:56 -0400

    module 5

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -40,7 +40,7 @@ namespace WishList.Controllers
         {
             var item=_context.Items.FirstOrDefault(e=>e.Id==id);
 
-            _context.Items.Remove(item)
+            _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
